058f36603a8d889664866283a3fdd33d4d103e1d
Dev Mehandru <78400278+d-e-v-14@users.noreply.github.com>
2021-07-23 15:57:02 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,24 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadAnimation("Walking Frame/walking_8.png","Walking Frame/walking_7.png","Walking Frame/walking_6.png","Walking Frame/walking_5.png","Walking Frame/walking_4.png","Walking Frame/walking_3.png","Walking Frame/walking_2.png","Walking Frame/walking_1.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+        this.img = loadImage("bat/Bestman-01.png");
+        
+    }
+
+    display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        if(frameCount%200===0){
+          //  image(this.img,pos.x,pos.y+70,300,300);
+        }else{
+          animation(this.image,pos.x,pos.y+70);
+        }
+        
+    }
+}
